f70a72da8193cf0046d749ec339577265c19648f
Jiakun Zhao <jiakun.zhao@outlook.com>
2023-04-18 20:47:29 +0800

    chore: new config

--- src/main.ts
+++ src/main.ts
@@ -1,7 +1,12 @@
-import { createApp } from 'vue'
-import App from '~/App.vue'
-
 import 'uno.css'
 
-const app = createApp(App)
-app.mount('#app')
+const jsdelivr = 'https://cdn.jsdelivr.net/gh'
+const folder = '.start-page'
+const root = location.pathname === '/'
+    ? 'jiakun-zhao/start-page'
+    : location.pathname.slice(1)
+
+const config = await fetch(`${jsdelivr}/${root}/${folder}/config.json`).then(res => res.json())
+const wallpaper = `${jsdelivr}/${root}/${folder}/${config.wallpaper}`
+
+console.log(config, wallpaper)
--- vite.config.ts
+++ vite.config.ts
@@ -1,13 +1,11 @@
 import { URL, fileURLToPath } from 'node:url'
 import { defineConfig } from 'vite'
 
-import Vue from '@vitejs/plugin-vue'
 import UnoCSS from 'unocss/vite'
 import { presetAttributify, presetUno } from 'unocss'
 
 export default defineConfig({
     plugins: [
-        Vue(),
         UnoCSS({
             presets: [
                 presetUno({ preflight: false }),
@@ -17,8 +15,5 @@ export default defineConfig({
     ],
     resolve: {
         alias: { '~': fileURLToPath(new URL('./src', import.meta.url)) },
-    },
-    server: {
-        host: true,
     },
 })
