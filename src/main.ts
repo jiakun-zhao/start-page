fcf1059768e8554a11468c45bde9556e393d494d
Jiakun Zhao <jiakun.zhao@outlook.com>
2023-04-18 20:51:29 +0800

    chore: new default wallpaper

--- src/main.ts
+++ src/main.ts
@@ -6,7 +6,7 @@ const root = location.pathname === '/'
     ? 'jiakun-zhao/start-page'
     : location.pathname.slice(1)
 
-const config = await fetch(`${jsdelivr}/${root}/${folder}/config.json`).then(res => res.json())
-const wallpaper = `${jsdelivr}/${root}/${folder}/${config.wallpaper}`
+const config = await fetch(`${jsdelivr}/${root}@latest/${folder}/config.json`).then(res => res.json())
+const wallpaper = `${jsdelivr}/${root}@latest/${folder}/${config.wallpaper}`
 
 console.log(config, wallpaper)
